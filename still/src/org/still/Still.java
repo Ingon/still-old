9c960ad90296e03f39aa23d674e98bd0c64732d2
sungon <sungon@31d3e170-832d-11dd-b774-4f160e1b508b>
2008-09-15 14:16:31 +0000

    basic echo repl

--- still/src/org/still/Still.java
+++ still/src/org/still/Still.java
@@ -1,7 +1,33 @@
 package org.still;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
 public class Still {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Still");
+	public static void main(String[] args) throws Exception {
+		System.out.println("Welcome to Still.");
+		repl(System.out, System.in);
+	}
+
+	private static void repl(OutputStream os, InputStream is) throws Exception {
+		PrintWriter out = new PrintWriter(os);
+		BufferedReader in = new BufferedReader(new InputStreamReader(is));
+		
+		do {
+			out.print("> ");
+			out.flush();
+			
+			String line = in.readLine();
+			
+			out.println(eval(line));
+			out.flush();
+		} while(true);
+	}
+
+	private static String eval(String line) {
+		return line;
 	}
 }
